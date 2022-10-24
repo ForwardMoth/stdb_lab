d642314bf8503ea160b669085a182324116a53da
gpnwln <gapanovichvalentin@gmail.com>
2022-10-25 02:55:52 +0300

    5,6,7 tasks

--- lab2.py
+++ lab2.py
@@ -12,11 +12,11 @@ df.rename(columns={'iyear': 'year', 'imonth': 'month', 'iday': 'day'}, inplace=T
 
 # 2 задание
 df2 = df.loc[:, ["year", "month", "day", "country_txt", "region_txt", "latitude", "longitude"]]
-#print(df2)
+# print(df2)
 
 df2 = df2.loc[(df2["month"] != 0) & (df2['day'] != 0)]
 df2['accident_date'] = pd.to_datetime(df2.loc[:, ["year", "month", "day"]].astype(str))
-#print(df2)
+# print(df2)
 
 # 3 задание
 columns = list(df2.columns)
@@ -31,12 +31,37 @@ df3.sort_values(by=new_column_order[:-1], inplace=True)
 # print(df3)
 
 # 4 задание
-countries_count = df3['country_txt'].value_counts()
+month_count = df3['country_txt'].value_counts()  # сразу делает по убыванию
+df_countries_count = pd.DataFrame({'country_txt': month_count.index, 'count': month_count.values})
+# print(df_countries_count)
 
-df_countries_count = pd.DataFrame({'country_txt': countries_count.index, 'count': countries_count.values})
-print(df_countries_count)
-
-# df4 = pd.merge(df3, df_countries_count)
+df4 = df3.merge(df_countries_count, how='right', on=['country_txt'])
 # print(df4)
 
 # 5 задание
+stat_analysis = df4.describe().reset_index()
+stat_analysis.rename(columns={'index': 'parameters'}, inplace=True)
+# count - кол-во непустых элементов, mean - среднее, std - среднеквадратичное отклонение
+# print(stat_analysis)
+# print(df4.mean(numeric_only=True))
+
+quantile = df4.quantile(q=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+# print(quantile)
+
+# 6 задание
+first_quantile = stat_analysis.iloc[4, 6]
+# print(first_quantile)
+third_quantile = stat_analysis.iloc[6, 6]
+# print(third_quantile)
+
+df_234quantile = df4[df4["count"] > first_quantile]
+df6 = df_234quantile[df_234quantile["count"] < third_quantile]
+# print(df6)
+
+# 7 задание
+month_count = df6['month'].value_counts()  # сразу делает по убыванию
+df_month_count = pd.DataFrame({'month': month_count.index, 'month_count': month_count.values})
+# print(df_month_count)
+
+df7 = df6.merge(df_month_count, how='right', on=['month'])
+# print(df7)
