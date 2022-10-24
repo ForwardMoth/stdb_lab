443cad641934e7c76795a46b796f4d8e32a0a150
gpnwln <gapanovichvalentin@gmail.com>
2022-10-24 18:46:44 +0300

    comments

--- lab2.py
+++ lab2.py
@@ -5,43 +5,38 @@ pd.set_option('display.width', 2000)
 
 # 1 задание
 df = pd.read_csv("task/globalterrorismdb_0718dist.csv", encoding='ISO-8859-1', low_memory=False)
-
 # print(df.head())
 
 df.rename(columns={'iyear': 'year', 'imonth': 'month', 'iday': 'day'}, inplace=True)
+# print(df.head())
 
 # 2 задание
-new_df = df.loc[:, ["year", "month", "day", "country_txt", "region_txt", "latitude", "longitude"]]
+df2 = df.loc[:, ["year", "month", "day", "country_txt", "region_txt", "latitude", "longitude"]]
+#print(df2)
 
-new_df = new_df.loc[(new_df["month"] != 0) & (new_df['day'] != 0)]
-
-new_df['accident_date'] = pd.to_datetime(new_df.loc[:, ["year", "month", "day"]].astype(str))
-
-# print(new_df)
+df2 = df2.loc[(df2["month"] != 0) & (df2['day'] != 0)]
+df2['accident_date'] = pd.to_datetime(df2.loc[:, ["year", "month", "day"]].astype(str))
+#print(df2)
 
 # 3 задание
-
-columns = list(new_df.columns)
-
+columns = list(df2.columns)
 new_column_order = columns[3:5] + columns[:3] + columns[-3:-1] + columns[-1:]
 
-new_df = new_df.reindex(columns=new_column_order)
-
-new_df.sort_values(by=new_column_order[:-1], inplace=True)
+df3 = df2.reindex(columns=new_column_order)
+df3.sort_values(by=new_column_order[:-1], inplace=True)
+# print(df3)
 
 # ascending_order = [False, False, True, True, True, False, False]
-# new_df.sort_values(by=new_column_order[:-1], ascending=ascending_order, inplace=True)
-
-# print(new_df)
+# df3.sort_values(by=new_column_order[:-1], ascending=ascending_order, inplace=True)
+# print(df3)
 
 # 4 задание
-
-countries_count = new_df['country_txt'].value_counts()
+countries_count = df3['country_txt'].value_counts()
 
 df_countries_count = pd.DataFrame({'country_txt': countries_count.index, 'count': countries_count.values})
-
 print(df_countries_count)
 
-# df4 = pd.merge(new_df, df_countries_count)
+# df4 = pd.merge(df3, df_countries_count)
 # print(df4)
 
+# 5 задание
